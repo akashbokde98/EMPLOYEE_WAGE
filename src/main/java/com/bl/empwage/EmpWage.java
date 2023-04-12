046f7119bf696a3d9cfd18571a647e262cb9fe6e
akashbokde98 <akashbokde98@gmail.com>
2023-04-12 15:33:09 +0530

    Added: Checking Attedance of Employee

--- src/main/java/com/bl/empwage/EmpWage.java
+++ src/main/java/com/bl/empwage/EmpWage.java
@@ -1,9 +1,19 @@
 package com.bl.empwage;
-
+import java.lang.Math;
 public class EmpWage
 {
     public static void main(String[] args)
     {
         System.out.println("Welcome To Employee Wage Computation Program!");
+        //To Check Employee Present or Absent.
+        int empAttendance = (int)((Math.random() * 10) % 2);
+        if (empAttendance == 1)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
     }
 }
