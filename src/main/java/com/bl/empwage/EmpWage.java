7e0040b03e1aea67353a3e14105e72eda7d1357a
akashbokde98 <akashbokde98@gmail.com>
2023-04-12 16:42:29 +0530

    Added:Employee Wage using Switch Case

--- src/main/java/com/bl/empwage/EmpWage.java
+++ src/main/java/com/bl/empwage/EmpWage.java
@@ -1,5 +1,6 @@
 package com.bl.empwage;
 import java.lang.Math;
+
 public class EmpWage
 {
     public static void main(String[] args)
@@ -10,22 +11,20 @@ public class EmpWage
 
         System.out.println("Welcome To Employee Wage Computation Program!");
         //Checking Employee Present or Absent.
-        int empAttendance = (int)((Math.random() * 10) % 3);
-        if (empAttendance == 1)
+        switch ((int) ((Math.random() * 10) % 3))
         {
-            System.out.println("Employee is Present Full Time");
-            int dailyWage = wagePerHour * fullDayHour; //Calculating Full Time Daily Wage.
-            System.out.print("Daily Employee Wage:" + dailyWage);
-        }
-        else if (empAttendance == 2)
-        {
-            System.out.println("Employee is Present Part Time");
-            int dailyWage = wagePerHour * partTimeHour; //Calculating Part Time Daily Wage.
-            System.out.print("Daily Employee Wage:" + dailyWage);
-        }
-        else
-        {
-            System.out.println("Employee is Absent");
+            case 1 -> {
+                System.out.println("Employee is Present Full Time");
+                int dailyWage = wagePerHour * fullDayHour; //Calculating Full Time Daily Wage.
+                System.out.print("Daily Employee Wage:" + dailyWage);
+            }
+            case 2 -> {
+                System.out.println("Employee is Present Part Time");
+                int dailyWage = wagePerHour * partTimeHour; //Calculating Part Time Daily Wage.
+                System.out.print("Daily Employee Wage:" + dailyWage);
+            }
+            default -> //if all cases are not matched.
+                    System.out.println("Employee is Absent");
         }
     }
 }
