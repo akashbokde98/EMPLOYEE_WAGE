516789e4f049ae609f22a89c7730b1dc6d9efb12
akashbokde98 <akashbokde98@gmail.com>
2023-04-12 16:09:33 +0530

    Added:Employee Part Time Wage

--- src/main/java/com/bl/empwage/EmpWage.java
+++ src/main/java/com/bl/empwage/EmpWage.java
@@ -6,14 +6,21 @@ public class EmpWage
     {
         int wagePerHour = 20;
         int fullDayHour = 8;
+        int partTimeHour = 4;
 
         System.out.println("Welcome To Employee Wage Computation Program!");
         //Checking Employee Present or Absent.
-        int empAttendance = (int)((Math.random() * 10) % 2);
+        int empAttendance = (int)((Math.random() * 10) % 3);
         if (empAttendance == 1)
         {
-            System.out.println("Employee is Present");
-            int dailyWage = wagePerHour * fullDayHour; //Calculating Daily Wage.
+            System.out.println("Employee is Present Full Time");
+            int dailyWage = wagePerHour * fullDayHour; //Calculating Full Time Daily Wage.
+            System.out.print("Daily Employee Wage:" + dailyWage);
+        }
+        else if (empAttendance == 2)
+        {
+            System.out.println("Employee is Present Part Time");
+            int dailyWage = wagePerHour * partTimeHour; //Calculating Part Time Daily Wage.
             System.out.print("Daily Employee Wage:" + dailyWage);
         }
         else
