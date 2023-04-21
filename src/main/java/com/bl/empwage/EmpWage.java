28db191aa4d8a06d0516f1a94ef0d68e5006a4e0
akashbokde98 <akashbokde98@gmail.com>
2023-04-21 12:08:56 +0530

    Added:Employee Wage Of Multiple Companies

--- src/main/java/com/bl/empwage/EmpWage.java
+++ src/main/java/com/bl/empwage/EmpWage.java
@@ -1,47 +1,42 @@
 package com.bl.empwage;
 import java.lang.Math;
+import java.util.Scanner;
 
 public class EmpWage
 {
-    //class Variables
-    int wagePerHour = 20;
+    //Common Data Of All Companies.
     int fullDayHour = 8;
     int partTimeHour = 4;
-    int WorkingDayPerMonth = 20;
     int totalDaysOfMonth = 30;
-    int totalWageOfMonth = 0;
-    int day = 0;
-    int totalWorkingDay = 0;
-    int totalWorkingHourForMonth = 100;
     int totalDailyWorkingHour = 0;
+    int totalWorkingDay = 0;
+    int totalWageOfMonth = 0;
+
+
     // Method To Calculate Total Employee Wage.
-    int calculateEmpWage()
+    int calculateEmpWage(int workingDayPerMonth, int totalWorkingHourForMonth, int wagePerHour)
     {
-        while (day < totalDaysOfMonth && totalWorkingDay < WorkingDayPerMonth && totalDailyWorkingHour < totalWorkingHourForMonth)
+        int day = 0;
+        while (day < totalDaysOfMonth && totalWorkingDay < workingDayPerMonth && totalDailyWorkingHour < totalWorkingHourForMonth)
         {
             //Checking Employee Present or Absent.
             switch ((int) ((Math.random() * 10) % 3))
             {
                 case 1: {
-                    System.out.println("Employee is Present Full Time");
                     int dailyWage = wagePerHour * fullDayHour; //Calculating Full Time Daily Wage.
-                    System.out.println("Daily Employee Wage:" + dailyWage);
                     totalWageOfMonth += dailyWage; //Calculating Total Monthly Wage.
                     totalWorkingDay++;
                     totalDailyWorkingHour++;
                     break;
                 }
                 case 2: {
-                    System.out.println("Employee is Present Part Time");
                     int dailyWage = wagePerHour * partTimeHour; //Calculating Part Time Daily Wage.
-                    System.out.println("Daily Employee Wage:" + dailyWage);
                     totalWageOfMonth += dailyWage; //Calculating Total Monthly Wage.
                     totalWorkingDay++;
                     totalDailyWorkingHour++;
                     break;
                 }
                 default://if all cases are not matched.
-                    System.out.println("Employee is Absent");
             }
             day++;
         }
@@ -51,9 +46,32 @@ public class EmpWage
     public static void main(String[] args)
     {
         System.out.println("Welcome To Employee Wage Computation Program!");
+        Scanner sc = new Scanner(System.in);
+        //Variable Declaration
+        int wagePerHour;
+        int WorkingDayPerMonth;
+        int totalWorkingHourForMonth;
+        int totalEmpWage;
 
-        EmpWage wage = new EmpWage();//Created obj of Class to call method.
-        int totalEmpWage = wage.calculateEmpWage();
-        System.out.print("Total Wage for a Month:" + totalEmpWage);
+        System.out.print("Enter Number Of Companies:");
+        int numOfCmp = sc.nextInt();
+        //Created obj of Class to call method.
+        EmpWage obj = new EmpWage();
+        //Iterating Over Numbers Of Company and Calculating Emp Wage.
+        for (int i = 1; i <= numOfCmp; i++)
+        {
+            System.out.println("Enter Data For Company-"+ i);
+            //Initializing
+            System.out.print("Wage Per Hour:");
+            wagePerHour = sc.nextInt();
+            System.out.print("Working Day Per Month:");
+            WorkingDayPerMonth = sc.nextInt();
+            System.out.print("Working Hour Per Month:");
+            totalWorkingHourForMonth = sc.nextInt();
+            //Calling and Storing Method
+            totalEmpWage = obj.calculateEmpWage(WorkingDayPerMonth, totalWorkingHourForMonth, wagePerHour);
+            System.out.println("Total Employee Wage:" + totalEmpWage);
+            System.out.println("------------------");
+        }
     }
 }
